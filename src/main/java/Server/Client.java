20173273bb1e91561ca530d49d4eefbf423efaaa
BajtekRogue <134713248+BajtekRogue@users.noreply.github.com>
2024-01-24 20:17:53 +0100

    Server v1.3

--- src/main/java/Server/Client.java
+++ src/main/java/Server/Client.java
@@ -1,6 +1,8 @@
 package Server;
 
+import GUI.GameModeSelectionGUI;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -8,8 +10,9 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
 
-public class Client2{
+public class Client1 extends Application{
 
     private final static int PORT = 1161;
     private static ObjectOutputStream outputStream;
@@ -26,10 +29,46 @@ public class Client2{
             outputStream = new ObjectOutputStream(socket.getOutputStream());
             inputStream = new ObjectInputStream(socket.getInputStream());
             String notification1 = receiveMessage();
+            GameModeSelectionGUI gameModeSelectionGUI = new GameModeSelectionGUI();
+            System.out.println(notification1);
 
-            if (notification1.equals("You have successfully connected to the server as player2")) {
+            if (notification1.equals("You have successfully connected to the server as player1")) {
+                CountDownLatch guiLatch = new CountDownLatch(1);
+
+                Platform.runLater(() -> {
+                    gameModeSelectionGUI.start(new Stage());
+                    guiLatch.countDown();  // This counts down the latch
+                });
+
+                // Wait for the GUI to be shown on the JavaFX application thread
+                guiLatch.await();
+                while (gameModeSelectionGUI.isWindowOpen()) {
+                    Thread.sleep(1000);  // Adjust the sleep time as needed
+                }
+
+                String gameMode = gameModeSelectionGUI.getSelectedGameMode();
+                System.out.println("Selected game mode: " + gameMode);
+                sendMessage(gameMode);
+
+                if(gameMode.equals("Human")){
+                    System.out.println("Waiting for player2 to join...");
+                    String notification2 = receiveMessage();
+                    System.out.println(notification2);
+                    if(notification2.equals("Player2 has successfully connected")){
+                        playAgainstHuman();
+                    }
+                }else{
+                    playAgainstBot();
+                }
+            }
+            else if (notification1.equals("You have successfully connected to the server as player2")) {
                 playAgainstHuman();
             }
+
+
+
+
+
 
 
         }catch(IOException e) {
@@ -43,6 +82,8 @@ public class Client2{
     }
 
 
+
+
     private static String receiveMessage() throws IOException, ClassNotFoundException {
         String notification = (String) inputStream.readObject();
         return notification;
@@ -50,6 +91,14 @@ public class Client2{
 
     private static void sendMessage(String message) throws IOException {
         outputStream.writeObject(message);
+    }
+
+    public static void playAgainstBot() throws InterruptedException {
+        while(true){
+            Thread.sleep(1000);
+            System.out.println("Bot");
+        }
+        //add
     }
 
     public static void playAgainstHuman() throws InterruptedException, IOException, ClassNotFoundException {
@@ -70,5 +119,10 @@ public class Client2{
 
             sendMessage(currentMove);
         }
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
     }
 }
